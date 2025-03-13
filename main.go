a58add5258556c6525602421b55c063cafc76762
Flarenzy <>
2025-03-13 13:28:30 +0100

    refactor to use TCP instead o files.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"net"
 	"os"
 	"strings"
 )
@@ -12,60 +13,45 @@ import (
 func getLinesChannel(f io.ReadCloser) <-chan string {
 	var buff [8]byte
 	stringChan := make(chan string)
-	var currentLineContent string
 	go func() {
 		defer close(stringChan)
+		currentLineContent := ""
 		for {
 			read, err := f.Read(buff[:])
 			if err != nil {
+				if currentLineContent != "" {
+					stringChan <- currentLineContent
+				}
 				if errors.Is(err, io.EOF) {
 					break
 				}
+				fmt.Printf("ERROR: %s\n", err)
+				return
 			}
 			splitLine := strings.Split(string(buff[:read]), "\n")
-			if len(splitLine) == 1 {
-				currentLineContent += splitLine[0]
-				continue
-			} else if len(splitLine) == 2 {
-				currentLineContent += splitLine[0]
-				stringChan <- currentLineContent
-				currentLineContent = splitLine[1]
-			} else {
-				for i, line := range splitLine {
-					if i == 0 {
-						stringChan <- currentLineContent + line
-						continue
-					}
-					if i == len(splitLine)-1 {
-						currentLineContent = line
-						break
-					}
-					stringChan <- line
-				}
+			for i := 0; i < len(splitLine)-1; i++ {
+				stringChan <- fmt.Sprintf("%s%s", currentLineContent, splitLine[i])
+				currentLineContent = ""
 			}
+			currentLineContent += splitLine[len(splitLine)-1]
 		}
-		if currentLineContent != "" {
-			stringChan <- currentLineContent
-		}
+
 		err := f.Close()
 		if err != nil {
 			fmt.Printf("Error closing file: %s\n", err)
 			return
 		}
+		fmt.Print("Connection closed.\n")
 	}()
 	return stringChan
 }
 
 func main() {
-	f, err := os.Open("messages.txt")
-	if err != nil {
-		panic(err)
-	}
 
 	logFile, err := os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		os.Exit(1)
 	}
 	logger := slog.New(slog.NewJSONHandler(logFile, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
@@ -73,14 +59,33 @@ func main() {
 	defer func(logFile *os.File) {
 		err = logFile.Close()
 		if err != nil {
-			fmt.Printf(err.Error())
+			fmt.Println(err.Error())
 		}
 	}(logFile)
+	listen, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		logger.Error("error binding tcp port", "err", err.Error())
+		os.Exit(1)
+	}
+	defer func(listen net.Listener) {
+		err = listen.Close()
+		if err != nil {
+			fmt.Printf("error closing TCP listener %s", err.Error())
+		}
+	}(listen)
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			logger.Error("error getting a new connection", "err", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Connection accepted", conn.RemoteAddr())
+		linesChan := getLinesChannel(conn)
+		logger.Info("Starting reading")
+		for line := range linesChan {
+			fmt.Println(line)
+		}
 
-	outChan := getLinesChannel(f)
-	logger.Info("Starting reading")
-	for line := range outChan {
-		fmt.Printf("read: %s\n", line)
 	}
 
 }
