46323425ab21dff87e0e3b1722d569ce29a281a0
Flarenzy <>
2025-03-29 18:37:04 +0100

    Validate header value before adding to map.

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -22,6 +22,9 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	}
 
 	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	if len(parts) != 2 {
+		return 0, false, fmt.Errorf("header must contain a colon")
+	}
 	key := string(parts[0])
 
 	if key != strings.TrimRight(key, " ") {
@@ -30,8 +33,10 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
 	value := bytes.TrimSpace(parts[1])
 	key = strings.TrimSpace(key)
-
-	h.Set(key, string(value))
+	if !isValidFieldName(key) {
+		return 0, false, fmt.Errorf("invalid char in field-name %s", key)
+	}
+	h.Set(strings.ToLower(key), string(value))
 	return idx + 2, false, nil
 }
 
@@ -42,4 +47,24 @@ func (h Headers) Set(key, value string) {
 func NewHeaders() Headers {
 	h := make(Headers)
 	return h
+}
+
+func isValidFieldName(s string) bool {
+	specialCharsMap := specialCharSet()
+	for _, c := range s {
+		_, ok := specialCharsMap[c]
+		if !(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z') && !(c >= '0' && c <= '9') && !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func specialCharSet() map[rune]bool {
+	specialChars := []rune{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+	specialCharsMap := make(map[rune]bool)
+	for _, c := range specialChars {
+		specialCharsMap[c] = true
+	}
+	return specialCharsMap
 }
